36a3a146dc0ec59facef485f9a23ad1050c46ab4
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-09 15:50:51 +0900

    feat: markdown -> html

--- useCases/cli/main.js
+++ useCases/cli/main.js
@@ -6,9 +6,9 @@
 
 const program = require("commander");
 const fs = require("fs"); // ファイルの読み書き
+const marked = require("marked");
 
 program.parse(process.argv);
-
 const filePath = program.args[0];
 
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
@@ -18,5 +18,7 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     process.exit(1);
     return;
   }
-  console.log(file);
+
+  const html = marked(file);
+  console.log(html);
 });
