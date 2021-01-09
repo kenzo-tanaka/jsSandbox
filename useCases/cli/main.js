936bc90d0694edeee634a47382e55923092ec847
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-09 15:46:54 +0900

    feat: fsモジュールでファイル読み書き

--- useCases/cli/main.js
+++ useCases/cli/main.js
@@ -5,7 +5,18 @@
 */
 
 const program = require("commander");
+const fs = require("fs"); // ファイルの読み書き
+
 program.parse(process.argv);
 
 const filePath = program.args[0];
-console.log(filePath);
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
