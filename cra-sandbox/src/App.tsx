52a4e73e6cfe94ce844891b630083f819d8082d6
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-10 22:09:19 +0900

    display styled todo form and todo list

--- cra-sandbox/src/App.tsx
+++ cra-sandbox/src/App.tsx
@@ -3,10 +3,22 @@ import React from "react";
 const App = () => {
   const todos: string[] = ["hoge", "hoge2", "hoge3"];
   return (
-    <div className="App">
-      {todos.map((todo) => {
-        return <p>{todo}</p>;
-      })}
+    <div className="todoapp">
+      <form id="js-form">
+        <input
+          id="js-form-input"
+          className="new-todo"
+          type="text"
+          placeholder="What need to be done?"
+        />
+      </form>
+      <div className="todo-list">
+        <ul>
+          {todos.map((todo) => {
+            return <li>{todo}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
