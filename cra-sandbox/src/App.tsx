ff83dcff8ebe4c1ac94bdcb8d41450c716022660
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-12 18:20:26 +0900

    feat: react-routerでページの切り替えを実装

--- cra-sandbox/src/App.tsx
+++ cra-sandbox/src/App.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import About from "./components/pages/About";
 import useTodo from "./hooks/useTodo";
+import { Route, Switch } from "react-router";
 
 const App = () => {
   const { todos, addTodo, toggleTodo, deleteTodo } = useTodo();
 
   return (
-    <div className="todoapp">
-      <TodoForm addTodo={addTodo} />
-      <div className="todo-list">
-        <TodoList
-          todos={todos}
-          toggleTodo={toggleTodo}
-          deleteTodo={deleteTodo}
-        />
-      </div>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <div className="todoapp">
+            <TodoForm addTodo={addTodo} />
+            <div className="todo-list">
+              <TodoList
+                todos={todos}
+                toggleTodo={toggleTodo}
+                deleteTodo={deleteTodo}
+              />
+            </div>
+          </div>
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
