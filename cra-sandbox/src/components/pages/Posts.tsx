7e46afdd22f95cfa12fd388e6e33983b43d7fbf9
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-13 10:12:18 +0900

    feat: Posts コンポーネントを追加

---
+++ cra-sandbox/src/components/pages/Posts.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Posts = () => {
+  return (
+    <div className="todoapp">
+      <div className="todo-list">
+        <ul>
+          <li>post</li>
+          <li>post</li>
+          <li>post</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
