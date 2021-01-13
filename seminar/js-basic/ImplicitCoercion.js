d07de9905a2b4b93a851aa54341318871789e0d6
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-13 14:44:43 +0900

    fix: jsprimer勉強会準備ファイル群を修正

--- seminar/js-basic/ImplicitCoercion.js
+++ seminar/js-basic/ImplicitCoercion.js
@@ -1,7 +1,20 @@
 // 暗黙的な型変換
-
 console.log(1 == "1"); // => true // "1"を数値の1へと暗黙的に型変換してから比較する
 console.log(1 + true); // => 2
+
+/*
+$ irb # rubyの場合
+irb(main):001:0> 1 == "1"
+=> false
+irb(main):002:0> 1 + true
+Traceback (most recent call last):
+        5: from /Users/tanakakenzou/.rbenv/versions/2.6.3/bin/irb:23:in `<main>'
+        4: from /Users/tanakakenzou/.rbenv/versions/2.6.3/bin/irb:23:in `load'
+        3: from /Users/tanakakenzou/.rbenv/versions/2.6.3/lib/ruby/gems/2.6.0/gems/irb-1.0.0/exe/irb:11:in `<top (required)>'
+        2: from (irb):2
+        1: from (irb):2:in `+'
+TypeError (true can't be coerced into Integer)
+*/
 
 // 厳密等価演算子を使う: 暗黙的な型変換をせずに比較をしてくれる
 console.log(1 === "1"); // => false
--- seminar/js-basic/PracticeVar.js
+++ seminar/js-basic/PracticeVar.js
@@ -5,6 +5,8 @@ console.log(x); // undefined
 var x = 2;
 console.log(x); // 変数の再定義ができてしまう
 
+/* --- --- --- */
+
 // 巻き上げが起こる
 console.log(x2); // 宣言をする前に参照しているが、エラーにならない
 var x2 = "hoge";
@@ -18,3 +20,8 @@ var x2 = "hoge";
 // 下記は宣言前に参照しているので、エラーになる
 // console.log(x3);
 // let x3 = "hoge";
+
+/* 
+ ECMASCriptでは後方互換性を重視しているので、var自体の挙動は変えずに
+ const, let が追加された。
+*/
